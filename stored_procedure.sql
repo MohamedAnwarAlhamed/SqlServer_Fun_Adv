4a74b744fcc8f6e1169ebd7a8160b9e5808b6577
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-29 18:07:06 +0300

    stored procedure 1

---
+++ stored_procedure.sql
@@ -0,0 +1,130 @@
+drop, alter
+create procedure name
+        @param1 type,
+        @param2 type output
+as
+begin
+        statements
+end 
+----------------------------
+create procedure Customer_total_purchases
+        @customer_id bigint,
+        @total_purchases decimal output
+        @sales_count decimal output
+---------------------------
+create procedure SalesDate
+as
+begin
+        select * from Sales;
+end
+
+exec SalesDate
+---------------------------
+create procedure SalesDate
+        @customerId bigint = null
+        @min_price decimal(7,2)
+        @max_price decimal(7,2) = 99999
+as
+begin
+        select * from Sales
+        where Total_price >= @min_price
+        and (CustomerId = @customerId or 
+                    @customerId is null)
+        and Total_price <= @max_price
+end
+
+exec SalesDate 10, 100.00
+---------------------------
+create procedure InsertUpdateOnDepartment
+        @DeptNo int,
+        @DeptName varchar(50),
+        @DeptParent int
+AS
+begin
+        if not exists (select * from Department where DeptNo = @DeptParent)
+                begin
+                    print 'Parent department does not exist' 
+                    return;
+                end
+        if not exists (select * from Department where DeptNo = @DeptNo)
+                begin
+                    insert into Department
+                    values (@DeptNo, @DeptName, @DeptParent)
+                end
+        else
+                begin
+                    update Department
+                    set DeptName = @DeptName,
+                    DeptParent = @DeptParent
+                    where DeptNo = @DeptNo
+                end
+
+end
+
+exec InsertUpdateOnDepartment 10, 'Sales', 1
+---------------------------
+create procedure GetEmployeeSalary
+        @EmployeeId bigint,
+        @salary decimal output
+as
+begin
+        declare @contract_type varchar(200)
+        select @contract_type = Contract_Type
+        from Employee
+        where EmployeeId = @EmployeeId
+        if @contract_type is NULL
+            begin
+                set @salary = 0
+                print 'Employee does not exist'
+            end
+        if @contract_type = 'FIXED'
+                select @salary = Monthly_Salary from Employee_Fixed
+                where EmployeeId = @EmployeeId
+        else
+                select @salary = Hour_salary * Working_Hour from Employee_Hourly
+                where EmployeeId = @EmployeeId
+end
+declare @salary decimal
+exec GetEmployeeSalary 1, @salary output
+print @salary
+---------------------------
+create procedure updateSaleProductsAsString
+        @saleId bigint,
+        --@products varchar(200)
+begin
+        declare @product_Name nvarchar(300), @Quantity decimal(7,2),
+                @productsString nvarchar(MAX) = ''
+        declare Products cursor
+             for select Product_Name, Quantity
+                from Sales_Products inner join Product
+                on Sales_Products.Product_Id = Product.Product_Id
+                where SaleId = @saleId
+        open Products
+        fetch next from Products into @product_Name, @Quantity
+        while @@fetch_status = 0 
+                begin
+                    set @productsString = @productsString + ',' + @product_Name + '(' + cast(@Quantity as varchar(10)) + ')'
+
+                        fetch next from Products into @product_Name, @Quantity
+                end
+                update Sales set ProductsString = @productsString
+                where SaleId = @saleId
+                --print @productsString
+                close Products
+                deallocate Products
+
+end
+
+exec updateSaleProductsAsString 1
+---------------------------
+--/////////////////////////
+--/////////////////////////
+create proc p2 @id int, @name varchar(50),@age
+as 
+declare @i int = (select st_id from student where st_id = @id)
+if(@age<30 and @i is null)
+insert into student (st_id, st_name, st_age) values (@id, @name, @age)
+else 
+print 'invalid age or id'
+
+exec p2 1, 'abc', 20
