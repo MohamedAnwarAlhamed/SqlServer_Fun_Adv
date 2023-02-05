8babee5b3541eabe06ffe61edf3b5002283fa7c9
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-05 22:35:53 +0300

    schema 1

---
+++ schema.sql
@@ -0,0 +1,14 @@
+create schema Alex
+alter schema Alex transfer topic
+select * from alex.topic
+
+create table topic(
+    id int,
+    desct varchar(50)
+)
+
+select * from topic
+create synonym x for -------.-------
+/*
+create user and login in sql server
+*/
