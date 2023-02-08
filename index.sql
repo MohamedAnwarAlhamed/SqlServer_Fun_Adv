1379908023f31d824508acf49088ca376041661e
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-09 00:37:04 +0300

    index 2

--- index.sql
+++ index.sql
@@ -6,4 +6,18 @@ on coures(crs_naame)
 
 select * from courses
 where crs_name = 'java'
+---------------------------------- 
+-- auto for pk an Qk
+create index IX_Customer_FName
+on Customer(First_name)
+
+drop index IX_Customer_FName on Customer
+
+create index IX_Customer_FName
+on Customer(First_name, last_name)
+
+create index IX_Customer_FName
+on Customer(First_name desc, last_name asc)
+include (email)
+
 
