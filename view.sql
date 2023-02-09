980da449ff29073805edc9c88980f2badc980017
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-09 17:33:20 +0300

    view 2

--- view.sql
+++ view.sql
@@ -53,5 +53,32 @@ from dbo.student
 where st_age between 20 and 30
 
 create unique clustered index idx on v7(st_id)
+------------------------
+create view Male 
+as
+select * from Employee 
+where Gender = 'M'
 
+select * from Male
+------------------------
+create view sales_vm (sale_id, sale_date, total_price, discount, final_price, customer_id,
+employee_id, customer_name, employee_name, employee_gender) as 
+select sales.*, customer.first_name + ' ' + customer.last_name, employee.first_name
+       + ' ' + employee.last_name,
+       case when gender = 'M' then 'ذكر' else 'أنثى' end
+       from sales join customer on sales.customer_id = customer.customer_id
+         join employee on sales.employee_id = employee.employee_id
+
+select * from sales_vm
+
+select customer_name from sales_vm
+where employee_name like '%Mir%'
+
+select customer_name from sales_vm 
+where year(sale_date) = 2015
+and month(sale_date) = 1
+
+select customer_name, sum(total_price)
+from sales_vm 
+group by customer_name
 
