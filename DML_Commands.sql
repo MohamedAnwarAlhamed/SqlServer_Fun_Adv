d705d0b4479e21f1720aeefca68e49004eb9d589
Mohamed_Anwar <alhamed202024@gmail.com>
2023-02-11 16:56:45 +0300

    DML 2

--- DML_Commands.sql
+++ DML_Commands.sql
@@ -1,2 +1,12 @@
 insert into department (dept_id, dept_name, dept_lacatoin) values (1, 'IT', 'alex')
 insert into department values (2, 'HR', 'cairo')
+
+delete from emlopyee 
+where gender = 'F'
+and age>60;
+
+update department set deptmanagerid = NULL
+
+delete from ProjectEmps;
+
+truncate table ProjectEmps;
